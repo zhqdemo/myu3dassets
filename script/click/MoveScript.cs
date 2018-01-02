364b97d04cd66306579f886571bd55ede74ce996
张青 <zhq_502502@126.com>
2018-01-02 08:27:38 +0800

    123
    123

---
+++ script/click/MoveScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	/// <summary>
+	/// 获取鼠标世界坐标
+	/// </summary>
+	/// <returns>The click position.</returns>
+	public static Vector3 getClickPosition(Transform transform){
+		Vector3 screenPos = Camera.main.WorldToScreenPoint(transform.position); // 目的获取z，在Start方法  
+		Vector3 mousePos = Input.mousePosition;  
+		mousePos.z = screenPos.z; // 这个很关键  
+		Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);  
+		return worldPos;   
+	}
+}
---
+++ script/user/UserAction.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserAction : MonoBehaviour {
+	public float speed = 1f;
+	private bool moving = false;
+	private Vector3 mousePosition;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		right2move ();
+	}
+
+	void right2move(){
+		if(Input.GetMouseButton(1)){
+			mousePosition = MoveScript.getClickPosition (transform);
+			//Vector3 p = MoveScript.getClickPosition (transform);
+			Debug.Log ((mousePosition-transform.position).magnitude);
+			//transform.position=Vector3.MoveTowards(transform.position,p,speed * Time.deltaTime);
+			//transform.position = p;
+			moving = true;
+		}
+		if(moving){
+			if((mousePosition-transform.position).magnitude<=0){
+				moving = false;
+			}
+			transform.position=Vector3.MoveTowards(transform.position,mousePosition,speed);
+		}
+	}
+
+}
