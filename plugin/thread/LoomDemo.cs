79a00fa545c64a91b79e0b437cfe4835001e3d7a
zhangqing <zhangqing@chenksoft.com>
2018-01-06 08:13:22 +0800

    加入各种内库

---
+++ plugin/thread/LoomDemo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+
+public class LoomDemo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	private void readServerMsg(){
+		//启用线程
+		Loom.RunAsync(() =>{
+			new Thread (theadThread).Start ();
+		});
+		//new Thread (theadThread).Start ();
+	}
+	/// <summary>
+	/// 消息读取线程
+	/// </summary>
+	private void theadThread(){
+		
+	}
+}
